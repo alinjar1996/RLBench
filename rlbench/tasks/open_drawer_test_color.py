a6b1fc6f4394b5ea3e15d465f0a0be9b2e587718
Mohit Shridhar <mohit.appi@gmail.com>
2022-08-05 14:38:04 -0700

    randomized colors

--- rlbench/tasks/open_drawer_test_color.py
+++ rlbench/tasks/open_drawer_test_color.py
@@ -10,7 +10,7 @@ from rlbench.const import colors
 
 class OpenDrawerTestColor(Task):
 
-    DRAWER_COLORS = [colors[19], colors[19], colors[19]]
+    DRAWER_COLORS = [colors[18], colors[18], colors[18]]
 
     def init_task(self) -> None:
         self._options = ['bottom', 'middle', 'top']

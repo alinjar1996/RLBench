87e7a009fab7d463754c199fc612822dbd8f3fd8
Markus Grotz <grotz@uw.edu>
2022-09-11 16:46:29 -0700

    refactor dataset_generator

--- tools/dataset_generator.py
+++ tools/dataset_generator.py
@@ -48,120 +48,92 @@ def check_and_make(dir):
 
 
 def save_demo(demo, example_path, variation):
+    """
+    ..fixme:: point clouds are not stored?
+    """
+
+
+    data_names = [
+    'left_shoulder_rgb',
+    'left_shoulder_depth',
+    'left_shoulder_point_cloud',
+    'left_shoulder_mask',
+
+    'right_shoulder_rgb',
+    'right_shoulder_depth',
+    'right_shoulder_point_cloud',
+    'right_shoulder_mask',
+
+    'overhead_rgb',
+    'overhead_depth',
+    'overhead_point_cloud',
+    'overhead_mask',
+
+    'wrist_rgb',
+    'wrist_depth',
+    'wrist_point_cloud',
+    'wrist_mask',
+
+    'front_rgb',
+    'front_depth',
+    'front_point_cloud',
+    'front_mask'
+    ]
+
+    data_types = ['rgb', 'depth', 'point_cloud', 'mask'] * 5
+
+
+    data_paths =[LEFT_SHOULDER_RGB_FOLDER, 
+                LEFT_SHOULDER_DEPTH_FOLDER,
+                LEFT_SHOULDER_MASK_FOLDER,
+                '',
+                RIGHT_SHOULDER_RGB_FOLDER,
+                RIGHT_SHOULDER_DEPTH_FOLDER,
+                RIGHT_SHOULDER_MASK_FOLDER, 
+                '',
+                OVERHEAD_RGB_FOLDER, 
+                OVERHEAD_DEPTH_FOLDER,
+                OVERHEAD_MASK_FOLDER, 
+                '',
+                WRIST_RGB_FOLDER,
+                WRIST_DEPTH_FOLDER,
+                WRIST_MASK_FOLDER, 
+                '',
+                FRONT_RGB_FOLDER,
+                FRONT_DEPTH_FOLDER,
+                FRONT_MASK_FOLDER, 
+                '']
+
+    data_paths = [os.path.join(example_path, p) for p in data_paths]
+
 
     # Save image data first, and then None the image data, and pickle
-    left_shoulder_rgb_path = os.path.join(
-        example_path, LEFT_SHOULDER_RGB_FOLDER)
-    left_shoulder_depth_path = os.path.join(
-        example_path, LEFT_SHOULDER_DEPTH_FOLDER)
-    left_shoulder_mask_path = os.path.join(
-        example_path, LEFT_SHOULDER_MASK_FOLDER)
-    right_shoulder_rgb_path = os.path.join(
-        example_path, RIGHT_SHOULDER_RGB_FOLDER)
-    right_shoulder_depth_path = os.path.join(
-        example_path, RIGHT_SHOULDER_DEPTH_FOLDER)
-    right_shoulder_mask_path = os.path.join(
-        example_path, RIGHT_SHOULDER_MASK_FOLDER)
-    overhead_rgb_path = os.path.join(
-        example_path, OVERHEAD_RGB_FOLDER)
-    overhead_depth_path = os.path.join(
-        example_path, OVERHEAD_DEPTH_FOLDER)
-    overhead_mask_path = os.path.join(
-        example_path, OVERHEAD_MASK_FOLDER)
-    wrist_rgb_path = os.path.join(example_path, WRIST_RGB_FOLDER)
-    wrist_depth_path = os.path.join(example_path, WRIST_DEPTH_FOLDER)
-    wrist_mask_path = os.path.join(example_path, WRIST_MASK_FOLDER)
-    front_rgb_path = os.path.join(example_path, FRONT_RGB_FOLDER)
-    front_depth_path = os.path.join(example_path, FRONT_DEPTH_FOLDER)
-    front_mask_path = os.path.join(example_path, FRONT_MASK_FOLDER)
 
-    check_and_make(left_shoulder_rgb_path)
-    check_and_make(left_shoulder_depth_path)
-    check_and_make(left_shoulder_mask_path)
-    check_and_make(right_shoulder_rgb_path)
-    check_and_make(right_shoulder_depth_path)
-    check_and_make(right_shoulder_mask_path)
-    check_and_make(overhead_rgb_path)
-    check_and_make(overhead_depth_path)
-    check_and_make(overhead_mask_path)
-    check_and_make(wrist_rgb_path)
-    check_and_make(wrist_depth_path)
-    check_and_make(wrist_mask_path)
-    check_and_make(front_rgb_path)
-    check_and_make(front_depth_path)
-    check_and_make(front_mask_path)
+
+    for d in data_paths:
+        check_and_make(d)
 
     for i, obs in enumerate(demo):
-        left_shoulder_rgb = Image.fromarray(obs.left_shoulder_rgb)
-        left_shoulder_depth = utils.float_array_to_rgb_image(
-            obs.left_shoulder_depth, scale_factor=DEPTH_SCALE)
-        left_shoulder_mask = Image.fromarray(
-            (obs.left_shoulder_mask * 255).astype(np.uint8))
-        right_shoulder_rgb = Image.fromarray(obs.right_shoulder_rgb)
-        right_shoulder_depth = utils.float_array_to_rgb_image(
-            obs.right_shoulder_depth, scale_factor=DEPTH_SCALE)
-        right_shoulder_mask = Image.fromarray(
-            (obs.right_shoulder_mask * 255).astype(np.uint8))
-        overhead_rgb = Image.fromarray(obs.overhead_rgb)
-        overhead_depth = utils.float_array_to_rgb_image(
-            obs.overhead_depth, scale_factor=DEPTH_SCALE)
-        overhead_mask = Image.fromarray(
-            (obs.overhead_mask * 255).astype(np.uint8))
-        wrist_rgb = Image.fromarray(obs.wrist_rgb)
-        wrist_depth = utils.float_array_to_rgb_image(
-            obs.wrist_depth, scale_factor=DEPTH_SCALE)
-        wrist_mask = Image.fromarray((obs.wrist_mask * 255).astype(np.uint8))
-        front_rgb = Image.fromarray(obs.front_rgb)
-        front_depth = utils.float_array_to_rgb_image(
-            obs.front_depth, scale_factor=DEPTH_SCALE)
-        front_mask = Image.fromarray((obs.front_mask * 255).astype(np.uint8))
 
-        left_shoulder_rgb.save(
-            os.path.join(left_shoulder_rgb_path, IMAGE_FORMAT % i))
-        left_shoulder_depth.save(
-            os.path.join(left_shoulder_depth_path, IMAGE_FORMAT % i))
-        left_shoulder_mask.save(
-            os.path.join(left_shoulder_mask_path, IMAGE_FORMAT % i))
-        right_shoulder_rgb.save(
-            os.path.join(right_shoulder_rgb_path, IMAGE_FORMAT % i))
-        right_shoulder_depth.save(
-            os.path.join(right_shoulder_depth_path, IMAGE_FORMAT % i))
-        right_shoulder_mask.save(
-            os.path.join(right_shoulder_mask_path, IMAGE_FORMAT % i))
-        overhead_rgb.save(
-            os.path.join(overhead_rgb_path, IMAGE_FORMAT % i))
-        overhead_depth.save(
-            os.path.join(overhead_depth_path, IMAGE_FORMAT % i))
-        overhead_mask.save(
-            os.path.join(overhead_mask_path, IMAGE_FORMAT % i))
-        wrist_rgb.save(os.path.join(wrist_rgb_path, IMAGE_FORMAT % i))
-        wrist_depth.save(os.path.join(wrist_depth_path, IMAGE_FORMAT % i))
-        wrist_mask.save(os.path.join(wrist_mask_path, IMAGE_FORMAT % i))
-        front_rgb.save(os.path.join(front_rgb_path, IMAGE_FORMAT % i))
-        front_depth.save(os.path.join(front_depth_path, IMAGE_FORMAT % i))
-        front_mask.save(os.path.join(front_mask_path, IMAGE_FORMAT % i))
+        for name, dtype, path in zip(data_names, data_types, data_paths):
+            data = getattr(obs, name, None)
+            if dtype == 'rgb':                
+                data = Image.fromarray(data)
+            elif dtype == 'depth':
+                data = utils.float_array_to_rgb_image(data, scale_factor=DEPTH_SCALE)
+            elif dtype == 'point_cloud':
+                continue
+            elif dtype == 'mask':
+                data = Image.fromarray((data * 255).astype(np.uint8))
+            else:
+                raise Exception('Invalid data type')
 
-        # We save the images separately, so set these to None for pickling.
-        obs.left_shoulder_rgb = None
-        obs.left_shoulder_depth = None
-        obs.left_shoulder_point_cloud = None
-        obs.left_shoulder_mask = None
-        obs.right_shoulder_rgb = None
-        obs.right_shoulder_depth = None
-        obs.right_shoulder_point_cloud = None
-        obs.right_shoulder_mask = None
-        obs.overhead_rgb = None
-        obs.overhead_depth = None
-        obs.overhead_point_cloud = None
-        obs.overhead_mask = None
-        obs.wrist_rgb = None
-        obs.wrist_depth = None
-        obs.wrist_point_cloud = None
-        obs.wrist_mask = None
-        obs.front_rgb = None
-        obs.front_depth = None
-        obs.front_point_cloud = None
-        obs.front_mask = None
+            if data is not None:
+                data.save(os.path.join(path, IMAGE_FORMAT % i))
+            print(name)
+
+        for name in data_names:
+            setattr(obs, name, None)
 
     # Save the low-dimension data
     with open(os.path.join(example_path, LOW_DIM_PICKLE), 'wb') as f:

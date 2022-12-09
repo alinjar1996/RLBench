2c29bfac420ab4c786972dad724d1121484b9f1d
Markus Grotz <grotz@uw.edu>
2022-12-09 18:31:34 +0100

    add support for multiple colors

--- rlbench/bimanual_tasks/handover_item.py
+++ rlbench/bimanual_tasks/handover_item.py
@@ -2,14 +2,12 @@ from collections import defaultdict
 from typing import List, Tuple
 
 import numpy as np
-from pyrep.objects.dummy import Dummy
-from pyrep.objects.joint import Joint
 from pyrep.objects.proximity_sensor import ProximitySensor
 from pyrep.objects.shape import Shape
 from rlbench.backend.conditions import DetectedCondition
-from rlbench.backend.task import Task
 from rlbench.backend.task import BimanualTask
-
+from rlbench.backend.spawn_boundary import SpawnBoundary
+from pyrep.objects.dummy import Dummy
 
 colors = [
     ('green', (0.0, 0.5, 0.0)),
@@ -27,28 +25,32 @@ class HandoverItem(BimanualTask):
 
     def init_task(self) -> None:
 
-
-        self.items = [Shape(f'item{i}') for i in range(3)]
-        self.register_graspable_objects(self.items)
+        self.items = [Shape(f'item{i}') for i in range(5)]
+        self.register_graspable_objects(self.items[0])
 
         self.waypoint_mapping = defaultdict(lambda: 'left')
         self.waypoint_mapping.update({'waypoint0': 'right', 'waypoint5': 'right'})
+
+        #self.boundaries = SpawnBoundary('handover_item_boundary')
 
 
     def init_episode(self, index) -> List[str]:
 
         success_sensor = ProximitySensor('Panda_rightArm_gripper_attachProxSensor')
 
-
         color_name, color = colors[index]
         self.items[0].set_color(color)
-        
+
         remaining_colors = colors.copy()
         remaining_colors.remove((color_name, color))
         np.random.shuffle(remaining_colors)
 
         for i, item in  enumerate(self.items):
             item.set_color(remaining_colors[i][1])
+
+        #b = SpawnBoundary([self.boundaries])
+        #for item in self.items:
+        #    b.sample(item, min_distance=0.1)
 
 
         self.register_success_conditions(

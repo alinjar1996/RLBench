48d0304127ef4a3a6a9a403ecceef0660360ebfd
Markus Grotz <grotz@uw.edu>
2023-09-20 22:30:07 -0700

    update PyRep repository URL for github workflows

--- tools/dataset_generator_bimanual.py
+++ tools/dataset_generator_bimanual.py
@@ -25,35 +25,16 @@ from rlbench.backend import utils
 from rlbench.backend.const import *
 import numpy as np
 
-from absl import app
-from absl import flags
 import logging
 
-FLAGS = flags.FLAGS
+import rich_click as click
+from click_prompt import choice_option
+from click_prompt import filepath_option
 
-flags.DEFINE_string('save_path',
-                    '/tmp/rlbench_data/',
-                    'Where to save the demos.')
-flags.DEFINE_list('tasks', ["dual_push_buttons"], #, "coordinated_put_item_in_drawer_right", "coordinated_put_item_in_drawer"
-                  'The tasks to collect. If empty, all tasks are collected.')
-flags.DEFINE_list('image_size', [128, 128],
-                  'The size of the images tp save.')
-flags.DEFINE_enum('renderer',  'opengl3', ['opengl', 'opengl3'],
-                  'The renderer to use. opengl does not include shadows, '
-                  'but is faster.')
-flags.DEFINE_integer('processes', 1,
-                     'The number of parallel processes during collection.')
-flags.DEFINE_integer('episodes_per_task', 10,
-                     'The number of episodes to collect per task.')
-flags.DEFINE_integer('variations', -1,
-                     'Number of variations to collect per task. -1 for all.')
-flags.DEFINE_bool('all_variations', True,
-                  'Include all variations when sampling epsiodes')
-
-flags.DEFINE_bool('headless', True,
-                  'Hide the simulator window')
+IMAGE_SIZE = [128, 128]
 
 camera_names = ["over_shoulder_left", "over_shoulder_right", "overhead", "wrist_right", "wrist_left", "front"]
+
 
 def save_demo(demo, example_path, variation):
     data_types = ["rgb", "depth", "point_cloud", "mask"]
@@ -97,7 +78,7 @@ def save_demo(demo, example_path, variation):
         pickle.dump(variation, f)
 
 
-def run(i, lock, task_index, variation_count, results, file_lock, tasks):
+def run(i, lock, task_index, variation_count, results, file_lock, tasks, headless, save_path, episodes_per_task, variations):
     """Each thread will choose one task and variation, and then gather
     all the episodes_per_task for that variation."""
 
@@ -105,13 +86,11 @@ def run(i, lock, task_index, variation_count, results, file_lock, tasks):
     np.random.seed(None)
     num_tasks = len(tasks)
 
-    img_size = list(map(int, FLAGS.image_size))
+    img_size = list(map(int, IMAGE_SIZE))
     obs_config = ObservationConfig()
     obs_config.set_all(True)
 
     default_config_params = {"image_size": img_size, "depth_in_meters": False, "masks_as_one_channel": False}
-    if FLAGS.renderer == 'opengl':
-        default_config_params["render_mode"] = RenderMode.OPENGL
     camera_configs = {camera_name: CameraConfig(**default_config_params) for camera_name in camera_names}
     obs_config.camera_configs = camera_configs
     
@@ -120,7 +99,7 @@ def run(i, lock, task_index, variation_count, results, file_lock, tasks):
         action_mode=BimanualMoveArmThenGripper(BimanualJointVelocity(), BimanualDiscrete()),
         obs_config=obs_config,
         robot_setup=robot_setup,
-        headless=FLAGS.headless)
+        headless=headless)
  
     rlbench_env.launch()
 
@@ -140,8 +119,8 @@ def run(i, lock, task_index, variation_count, results, file_lock, tasks):
             t = tasks[task_index.value]
             task_env = rlbench_env.get_task(t)
             var_target = task_env.variation_count()
-            if FLAGS.variations >= 0:
-                var_target = np.minimum(FLAGS.variations, var_target)
+            if variations >= 0:
+                var_target = np.minimum(variations, var_target)
             if my_variation_count >= var_target:
                 # If we have reached the required number of variations for this
                 # task, then move on to the next task.
@@ -159,7 +138,7 @@ def run(i, lock, task_index, variation_count, results, file_lock, tasks):
         descriptions, obs = task_env.reset()
 
         variation_path = os.path.join(
-            FLAGS.save_path, task_env.get_name(),
+            save_path, task_env.get_name(),
             VARIATIONS_FOLDER % my_variation_count)
 
         os.makedirs(variation_path, exist_ok=True)
@@ -172,7 +151,7 @@ def run(i, lock, task_index, variation_count, results, file_lock, tasks):
         os.makedirs(episodes_path, exist_ok=True)
 
         abort_variation = False
-        for ex_idx in range(FLAGS.episodes_per_task):
+        for ex_idx in range(episodes_per_task):
             print('Process', i, '// Task:', task_env.get_name(),
                   '// Variation:', my_variation_count, '// Demo:', ex_idx)
             # ..todo: I think I might need to increase the variable
@@ -207,8 +186,7 @@ def run(i, lock, task_index, variation_count, results, file_lock, tasks):
     results[i] = tasks_with_problems
     rlbench_env.shutdown()
 
-
-def run_all_variations(i, lock, task_index, variation_count, results, file_lock, tasks):
+def run_all_variations(tasks, headless, save_path, episodes_per_task, variations):
     """Each thread will choose one task and variation, and then gather
     all the episodes_per_task for that variation."""
 
@@ -216,13 +194,11 @@ def run_all_variations(i, lock, task_index, variation_count, results, file_lock,
     np.random.seed(None)
     num_tasks = len(tasks)
 
-    img_size = list(map(int, FLAGS.image_size))
+    img_size = list(map(int, IMAGE_SIZE))
     obs_config = ObservationConfig()
     obs_config.set_all(True)
 
     default_config_params = {"image_size": img_size, "depth_in_meters": False, "masks_as_one_channel": False}
-    if FLAGS.renderer == 'opengl':
-        default_config_params["render_mode"] = RenderMode.OPENGL
     camera_configs = {camera_name: CameraConfig(**default_config_params) for camera_name in camera_names}
     obs_config.camera_configs = camera_configs
 
@@ -231,35 +207,26 @@ def run_all_variations(i, lock, task_index, variation_count, results, file_lock,
         action_mode=BimanualMoveArmThenGripper(BimanualJointVelocity(), BimanualDiscrete()),
         obs_config=obs_config,
         robot_setup=robot_setup,
-        headless=FLAGS.headless)
+        headless=headless)
 
     rlbench_env.launch()
 
     task_env = None
 
-    tasks_with_problems = results[i] = ''
 
-    while True:
-        # Figure out what task/variation this thread is going to do
-        # with lock:
-        if task_index.value >= num_tasks:
-            print('Process', i, 'finished')
-            break
-
-        t = tasks[task_index.value]
-        task_env = rlbench_env.get_task(t)
+    for task in tasks:
+        
+        task_env = rlbench_env.get_task(task)
         possible_variations = task_env.variation_count()
 
-        variation_path = os.path.join(
-            FLAGS.save_path, task_env.get_name(),
-            VARIATIONS_ALL_FOLDER)
+        variation_path = os.path.join(save_path, task_env.get_name(), VARIATIONS_ALL_FOLDER)
         os.makedirs(variation_path, exist_ok=True)
 
         episodes_path = os.path.join(variation_path, EPISODES_FOLDER)
         os.makedirs(episodes_path, exist_ok=True)
 
         abort_variation = False
-        for ex_idx in range(FLAGS.episodes_per_task):
+        for ex_idx in range(episodes_per_task):
             attempts = 10
             while attempts > 0:
                 try:
@@ -284,46 +251,51 @@ def run_all_variations(i, lock, task_index, variation_count, results, file_lock,
                     if attempts > 0:
                         continue
                     problem = (
-                        'Process %d failed collecting task %s (variation: %d, '
+                        'Failed collecting task %s (variation: %d, '
                         'example: %d). Skipping this task/variation.\n%s\n' % (
-                            i, task_env.get_name(), variation, ex_idx,
+                            task_env.get_name(), variation, ex_idx,
                             str(e))
                     )
                     print(problem)
-                    tasks_with_problems += problem
+                    #tasks_with_problems += problem
                     abort_variation = True
                     break
                 episode_path = os.path.join(episodes_path, EPISODE_FOLDER % ex_idx)
-                with file_lock:
-                    save_demo(demo, episode_path, variation)
+               
+                save_demo(demo, episode_path, variation)
 
-                    with open(os.path.join(
-                            episode_path, VARIATION_DESCRIPTIONS), 'wb') as f:
-                        pickle.dump(descriptions, f)
+                with open(os.path.join(
+                        episode_path, VARIATION_DESCRIPTIONS), 'wb') as f:
+                    pickle.dump(descriptions, f)
                 break
             if abort_variation:
                 break
 
-        # with lock:
-        task_index.value += 1
 
-    results[i] = tasks_with_problems
+    #results[i] = tasks_with_problems
     rlbench_env.shutdown()
 
 
-def main(argv):
-    
-    bimanual_task_files = [t.replace('.py', '') for t in os.listdir(task.BIMANUAL_TASKS_PATH)
-                  if t != '__init__.py' and t.endswith('.py')]
+from rlbench.backend.task import BIMANUAL_TASKS_PATH
 
-    if len(FLAGS.tasks) > 0:
-        for t in FLAGS.tasks:
-            if t not in bimanual_task_files:
-                raise ValueError('Task %s not recognised!.' % t)
-        bimanual_task_files = FLAGS.tasks
-    
+def get_bimanual_tasks():
+    return [t.replace('.py', '') for t in
+    os.listdir(BIMANUAL_TASKS_PATH) if t != '__init__.py' and t.endswith('.py')]
 
-    tasks = [task_file_to_task_class(t, True) for t in bimanual_task_files]
+
+@click.command()
+@filepath_option("--save_path", default="/tmp/rlbench_data/",  help="Where to save the demos.")
+@choice_option('--tasks', type=click.Choice(get_bimanual_tasks()), multiple=True, help='The tasks to collect. If empty, all tasks are collected.')
+@click.option("--episodes_per_task", default=10, help="The number of episodes to collect per task.")
+@click.option("--all_variations", is_flag=True, default=True, help="Include all variations when sampling epsiodes")
+@click.option("--variations", default=-1, help="Number of variations to collect per task. -1 for all.")
+@click.option("--headless", default=True, is_flag=True, help='Hide the simulator window')
+def main(save_path, tasks, episodes_per_task, all_variations, variations, headless):
+    
+    import logging
+    logging.basicConfig()
+
+    tasks = [task_file_to_task_class(t, True) for t in tasks]
 
     manager = Manager()
 
@@ -334,21 +306,22 @@ def main(argv):
     variation_count = manager.Value('i', 0)
     lock = manager.Lock()
 
-    os.makedirs(FLAGS.save_path, exist_ok=True)
+    os.makedirs(save_path, exist_ok=True)
 
     # run_all_variations(0, lock, task_index, variation_count, result_dict, file_lock, tasks)
-    run_fn = run_all_variations if FLAGS.all_variations else run
-    processes = [Process(
-        target=run_fn, args=(
-            i, lock, task_index, variation_count, result_dict, file_lock,
-            tasks))
-        for i in range(FLAGS.processes)]
-    [t.start() for t in processes]
-    [t.join() for t in processes]
 
-    print('Data collection done!')
-    for i in range(FLAGS.processes):
-        print(result_dict[i])
+    if all_variations: 
+        run_all_variations(0, lock, task_index, variation_count, result_dict, file_lock, tasks, headless, save_path, episodes_per_task, variations)
+    else:
+        run(0, lock, task_index, variation_count, result_dict, file_lock, tasks, headless, save_path, episodes_per_task, variations)
+   
+    #print(result_dict)
 
 if __name__ == '__main__':
-  app.run(main)
+  main()
+
+
+#from multiprocessing import Pool
+#with Pool(processes=8) as pool:
+#        pool.starmap(eval_fn, enumerate(tasks))
+

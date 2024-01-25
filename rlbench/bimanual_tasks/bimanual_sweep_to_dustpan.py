37c8afe055a682eca007e3871d800b4cf774acdd
Markus Grotz <grotz@uw.edu>
2024-01-25 08:26:17 -0800

    update bimanual_sweep_to_dustpan

--- rlbench/bimanual_tasks/bimanual_sweep_to_dustpan.py
+++ rlbench/bimanual_tasks/bimanual_sweep_to_dustpan.py
@@ -5,6 +5,8 @@ from rlbench.backend.task import Task
 from rlbench.backend.conditions import DetectedCondition
 from rlbench.backend.task import BimanualTask
 from collections import defaultdict
+from rlbench.backend.spawn_boundary import SpawnBoundary
+
 
 DIRT_NUM = 5
 
@@ -12,16 +14,21 @@ DIRT_NUM = 5
 class BimanualSweepToDustpan(BimanualTask):
 
     def init_task(self) -> None:
-        broom = Shape('broom')
         success_sensor = ProximitySensor('success')
-        dirts = [Shape('dirt' + str(i)) for i in range(DIRT_NUM)]
-        conditions = [DetectedCondition(dirt, success_sensor) for dirt in dirts]
-        self.register_graspable_objects([broom])
+        self.dirts = [Shape('dirt' + str(i)) for i in range(DIRT_NUM)]
+        conditions = [DetectedCondition(dirt, success_sensor) for dirt in self.dirts]
+        self.register_graspable_objects([Shape('broom'), Shape('Dustpan_4'),  Shape('Dustpan_5'), Shape('Dustpan_3')])
         self.register_success_conditions(conditions)
         self.waypoint_mapping = defaultdict(lambda: 'left')
-        self.waypoint_mapping.update({'waypoint5': 'right', 'waypoint6': 'right'})
+        self.waypoint_mapping.update({'waypoint5': 'right', 'waypoint6': 'right', 'waypoint7': 'right'})
 
     def init_episode(self, index: int) -> List[str]:
+
+        b = SpawnBoundary([Shape('dirt_boundary')])
+        b.clear()
+        for item in self.dirts:
+            b.sample(item, min_distance=0.01, ignore_collisions=True)
+
         return ['sweep dirt to dustpan',
                 'sweep the dirt up',
                 'use the broom to brush the dirt into the dustpan',
@@ -33,3 +40,6 @@ class BimanualSweepToDustpan(BimanualTask):
 
     def variation_count(self) -> int:
         return 1
+    
+    def is_static_workspace(self):
+        return True

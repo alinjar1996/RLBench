8a3105fb67f032299b8a8180f53b62bb7651903b
Markus Grotz <grotz@uw.edu>
2024-10-25 15:57:21 -0700

    rename coordinated_push_box to bimanual_push_box

--- rlbench/bimanual_tasks/bimanual_push_box.py
+++ rlbench/bimanual_tasks/bimanual_push_box.py
@@ -10,7 +10,7 @@ from rlbench.backend.conditions import DetectedCondition
 from rlbench.backend.task import Task
 from rlbench.backend.task import BimanualTask
 
-class CoordinatedPushBox(BimanualTask):
+class BimanualPushBox(BimanualTask):
 
     def init_task(self) -> None:
 
